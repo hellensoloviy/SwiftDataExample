e7adf853b6202de7eb3769b0f500836f01375368
Hellen S. <hellensoloviy@gmail.com>
2026-01-21 15:02:54 +0200

    Create Expense.swift

---
+++ SwiftDataTestApp/Expense.swift
@@ -0,0 +1,22 @@
+//
+//  Expense.swift
+//  SwiftDataTestApp
+//
+//  Created by Hellen Soloviy on 21.01.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Expense {
+    @Attribute(.unique) var name: String
+    var date: Date
+    var value: Double
+    
+    init(name: String, date: Date, value: Double) {
+        self.name = name
+        self.date = date
+        self.value = value
+    }
+}
