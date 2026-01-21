36b11348bfeba986682aec43559a7a72d38158d4
Hellen S <hellensoloviy@gmail.com>
2026-01-21 14:42:10 +0200

    Initial Commit

---
+++ SwiftDataTestApp/SwiftDataTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTestAppApp.swift
+//  SwiftDataTestApp
+//
+//  Created by Hellen Soloviy on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDataTestAppTests/SwiftDataTestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTestAppTests.swift
+//  SwiftDataTestAppTests
+//
+//  Created by Hellen Soloviy on 21.01.2026.
+//
+
+import Testing
+@testable import SwiftDataTestApp
+
+struct SwiftDataTestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
